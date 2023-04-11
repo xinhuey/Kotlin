7d301c6ce902869ffae2e2fc09ca647b1180cc8f
Xin Huey Wong <90855887+xinhuey@users.noreply.github.com>
2023-04-10 21:52:34 -0400

    Add files via upload

---
+++ happybday.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val age=5*365
+    val name="Rover"
+    //birthday message
+    println("Happy Birthday, ${name}!")
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
+    //birthday cake!
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+}
